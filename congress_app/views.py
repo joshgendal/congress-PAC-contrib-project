2d742078afd509b61fec499a6eb635415f8011c7
joshcodingdojo <jgendal@codingdojo.com>
2021-05-07 17:32:47 -0400

    get 1 state cids from open secrets

--- congress_app/views.py
+++ congress_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import requests
+openSecretsAPIKey = "a8551db7eb798ce16ca3413e4cb6a30d"
+getLegislatorsURL = "http://www.opensecrets.org/api/?method=getLegislators"
+opnSecretsUrl = "${getLegislatorsURL}&id=${code}&apikey=${openSecretsAPIKey}&output=json"
 
 # Create your views here.
+def index(request):
+  r = requests.get(f"{getLegislatorsURL}&id=FL&apikey={openSecretsAPIKey}&output=json")
+  cids = []
+  for i in r.json()["response"]["legislator"]:
+    cids.append(i['@attributes']['cid'])
+    # print(len(i['@attributes']['cid']))
+  print(len(cids))
+  return render(request, "index.html")
