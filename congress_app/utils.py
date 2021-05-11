89af961106aa96ee8ff6a00311c927c8148d08ed
joshcodingdojo <jgendal@codingdojo.com>
2021-05-11 12:44:33 -0400

    get candidate summaries. pushing code up for now to work on log and reg and will merge later

--- congress_app/utils.py
+++ congress_app/utils.py
@@ -3,6 +3,7 @@ from django.conf import settings
 openSecretsAPIKey = "a8551db7eb798ce16ca3413e4cb6a30d"
 getLegislatorsURL = "http://www.opensecrets.org/api/?method=getLegislators"
 opnSecretsUrl = "${getLegislatorsURL}&id=${code}&apikey=${openSecretsAPIKey}&output=json"
+candidate_summary_url = "https://www.opensecrets.org/api/?method=candSummary"
 
 """
 A function to get US State Code List
@@ -10,6 +11,7 @@ A function to get US State Code List
 Reads json file, packs state codes into list and returns it
 """
 def get_state_code_list():
+  print('GETTING STATE CODES')
   state_codes = []
   data = open(os.path.join(settings.BASE_DIR, 'congress_app/static/state_code_data.json'))
   data1 = json.load(data)
@@ -19,11 +21,26 @@ def get_state_code_list():
   return state_codes
   
 def get_cid_list(codes):
+  print('GETTING CID LIST')
   cid_list = []
   for code in codes:
     r = requests.get(f"{getLegislatorsURL}&id={code}&apikey={openSecretsAPIKey}&output=json")
     for i in r.json()["response"]["legislator"]:
       if isinstance(i, dict):
         cid_list.append(i["@attributes"]["cid"])
-        print(len(cid_list))
+  print('FINISHED CID LIST')
   return cid_list
+
+def get_candidate_summaries(cids):
+  print('GETTING CANDIDATE SUMMARY')
+  data = []
+  # r = requests.get(f"{candidate_summary_url}&cid={i}apikey={openSecretsAPIKey}&output=json")
+  for i in cids:
+    r = requests.get(f"{candidate_summary_url}&cid={i}&apikey={openSecretsAPIKey}&output=json")
+    if r.status_code == 200:
+      attributes = r.json()["response"]["summary"]["@attributes"]
+      data.append(attributes)
+      print(len(data))
+      # print("================================================================")
+  print('FINISHED CANDIDATE SUMMARY')
+  return data
--- congress_app/views.py
+++ congress_app/views.py
@@ -5,29 +5,34 @@ from django.shortcuts import render
 import requests
 import json
 import os
-from .utils import get_state_code_list, get_cid_list
+from .utils import get_state_code_list, get_cid_list, get_candidate_summaries
 openSecretsAPIKey = "a8551db7eb798ce16ca3413e4cb6a30d"
 getLegislatorsURL = "http://www.opensecrets.org/api/?method=getLegislators"
 opnSecretsUrl = "${getLegislatorsURL}&id=${code}&apikey=${openSecretsAPIKey}&output=json"
-
+candidate_summary_url = "https://www.opensecrets.org/api/?method=candSummary"
 # load state codes
 state_code_list = get_state_code_list()
 # get list of legislator unique id ("cid")
 cid_list = get_cid_list(state_code_list)
-
+# get list of dictionaries containing contribution data
+cand_summaries = get_candidate_summaries(cid_list)
+print(cand_summaries)
 
 
 # Create your views here.
 def index(request):
-  r = requests.get(f"{getLegislatorsURL}&id=FL&apikey={openSecretsAPIKey}&output=json")
-  cids = []
+  # r = requests.get(f"{candidate_summary_url}&cid=N00007360&apikey={openSecretsAPIKey}&output=json")
+  # print(r.json()["response"]["summary"]["@attributes"])
+  # attributes = r.json()["response"]["summary"]["@attributes"]
+  
+  # cids = []
 
   
 
 
-  for i in r.json()["response"]["legislator"]:
-    cids.append(i['@attributes']['cid'])
-    # print(len(i['@attributes']['cid']))
-  print(len(cids))
+  # for i in r.json()["response"]["legislator"]:
+  #   cids.append(i['@attributes']['cid'])
+  #   # print(len(i['@attributes']['cid']))
+  # print(len(cids))
   return render(request, "index.html")
 
